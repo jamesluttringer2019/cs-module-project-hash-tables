7b85b2f33665bee8e981858de9b990b25a448ff5
James Luttringer <jamesluttringer18@gmail.com>
2020-06-04 13:46:29 -0400

    markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,27 @@
 import random
-
+import re
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
-
-
-# TODO: construct 5 random sentences
-# Your code here
-
+words = words.split()
+table = {}
+for i, word in enumerate(words):
+    if word in table:
+        table[word].append(words[i+1])
+    else:
+        try:
+            table[word] = [words[i+1]]
+        except:
+            pass
+start_words = [w for w in table if w[0].isupper() or (w[0]=='"' and w[1].isupper())]
+stop_words = [w for w in table if w.endswith(('.', '."', '?', '?"', '!', '!"'))]
+for i in range(5):
+    word = random.choice(start_words)
+    s = word
+    while word:
+        word = random.choice(table[word])
+        s = s + " " + word
+        if word in stop_words:
+            break
+    print(s)
