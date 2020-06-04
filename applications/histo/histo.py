b0de5e248b83c891b736ca6c7f8f4e6c8e6f8ddb
James Luttringer <jamesluttringer18@gmail.com>
2020-06-04 14:25:36 -0400

    hist

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,22 @@
-# Your code here
+with open('robin.txt') as f:
+    words = f.read()
 
+punct = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split()
+words = words.lower()
+for char in words:
+    if char in punct:
+        words = words.replace(char, '')
+
+hist = {}
+for word in words.split():
+    if word in hist:
+        hist[word] += '#'
+    else:
+        hist[word] = '#'
+maxlen = 0
+for word in hist:
+    if len(word)>maxlen:
+        maxlen = len(word)
+
+for i in sorted(hist.items(), key=lambda a: (-len(a[1]),a[0])):
+    print(f'{i[0]:{maxlen}}  {i[1]}')
